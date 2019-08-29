abd2c100e7674480fd58ef6ede7c886021832ecf
Nathan S. Harris <nathansharris@gmail.com>
2019-08-28 22:22:37 -0600

    getting ready to add the substiution step

--- lgoUpdate.sh
+++ lgoUpdate.sh
@@ -48,8 +48,17 @@ echo "File to write: $OUTPUT"
 
 declare -A NEW 
 for i in `grep twoN b2.legofit | awk '{print $1"="$3}' `;
-    #do echo $i;
-    do export temp=`cut -f1 -d"=" $i`;
-    echo $temp
-    NEW["$temp"]=`cut -f2 -d"=" $i`;
+    do export temp=`echo $i | cut -f1 -d"="`;
+    NEW["$temp"]=`echo $i | cut -f2 -d"="`;
 done
+
+for i in `grep T b2.legofit | grep -v "#" |awk '{print $1"="$3}' `;
+    do export temp=`echo $i | cut -f1 -d"="`;
+    NEW["$temp"]=`echo $i | cut -f2 -d"="`;
+done
+
+echo ${!NEW[@]}
+echo ${NEW[@]}
+
+#for param in ${!NEW[@]};
+#    do 
