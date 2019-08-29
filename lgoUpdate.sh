9563e1cc21a751c5c0bf0b7e7132e7b95a8a548b
Nathan Harris <nathansharris@gmail.com>
2019-08-29 12:19:15 -0600

    This is working.

--- lgoUpdate.sh
+++ lgoUpdate.sh
@@ -45,20 +45,27 @@ echo "File to update: $LGO"
 echo "New values to use: $UPDATE"
 echo "File to write: $OUTPUT"
 
-
-declare -A NEW 
-for i in `grep twoN b2.legofit | awk '{print $1"="$3}' `;
-    do export temp=`echo $i | cut -f1 -d"="`;
-    NEW["$temp"]=`echo $i | cut -f2 -d"="`;
+declare -A NEW
+for i in `awk '{$1=$1;print}' b2.legofit | 
+        grep -e ^twoN -e ^m -e ^T | 
+        awk '{print $1"="$3}' `;
+    do NEW[`echo $i | cut -f1 -d"="`]=`echo $i | cut -f2 -d"="`;
 done
 
-for i in `grep T b2.legofit | grep -v "#" |awk '{print $1"="$3}' `;
-    do export temp=`echo $i | cut -f1 -d"="`;
-    NEW["$temp"]=`echo $i | cut -f2 -d"="`;
+#echo ${!NEW[@]}
+#echo ${NEW[@]}
+
+cp a.lgo $OUTPUT
+for param in "${!NEW[@]}";
+    #do echo $param ${NEW[$param]}
+    do sed -i 's/[^#]#.*//g' $OUTPUT #removes comments not in the header
+    
+    #change thre free time parameters
+    sed -i "s/^time.free.*$param[^A-Za-z].*/time  free $param = ${NEW[$param]}/g" $OUTPUT
+    
+    #change the population size parameters
+    sed -i "s/^twoN.*$param[^A-Za-z].*/twoN  free $param = ${NEW[$param]}/g" $OUTPUT
+
+    #change the mixFrac parameters
+    sed -i "s/^mixFrac.*[^A-Za-z]$param[^A-Za-z].*/mixFrac  free $param = ${NEW[$param]}/g" $OUTPUT
 done
-
-echo ${!NEW[@]}
-echo ${NEW[@]}
-
-#for param in ${!NEW[@]};
-#    do 
